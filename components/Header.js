6ebf22b2ef7e825585469d3e164a919b6f1e6caf
Alejandro Sánchez <alejandroslpz@gmail.com>
2020-08-06 09:46:38 -0500

    Agregado el logo

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,28 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  // Definir el routing
+
+  const router = useRouter();
+
+  const redireccionar = () => {
+    router.push("/");
+  };
+
   return (
-    <nav className="navbar is-ligth">
+    <nav className="navbar is-ligth is-fixed-top">
+      <div className="navbar-brand">
+        <div className="navbar-item">
+          <a className="title is-3" onClick={() => redireccionar()}>
+            <span className="has-text-primary">
+              <strong>Task</strong>
+            </span>
+            Manager
+          </a>
+        </div>
+      </div>
       <div className="navbar-end">
         <div className="navbar-item">
           <div className="buttons">
