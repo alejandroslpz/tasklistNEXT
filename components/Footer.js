ffe84d3633278e8ece2a86adbadf521cd5a99e79
Alejandro Sánchez <alejandroslpz@gmail.com>
2020-08-06 09:57:09 -0500

    Agregando contenedor para estilo

--- components/Footer.js
+++ components/Footer.js
@@ -2,14 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mt-4 pb-6">
-      <div className="content has-text-centered">
-        <p>Designed by</p>
-        <p>
-          <strong>Alejandro Sánchez</strong> © 2020
-        </p>
-      </div>
-    </footer>
+    <div className="container">
+      <footer className="mt-4 pb-6">
+        <div className="content has-text-centered">
+          <p>Designed by</p>
+          <p>
+            <strong>Alejandro Sánchez</strong> © 2020
+          </p>
+        </div>
+      </footer>
+    </div>
   );
 };
 
