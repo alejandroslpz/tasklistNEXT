c693376bce152fa8eea07dd4314fbf709cf9219b
Alejandro Sánchez <alejandroslpz@gmail.com>
2020-08-06 09:52:31 -0500

    DLibreria fontawesome

---
+++ components/Fontawesome.js
@@ -0,0 +1,13 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faHome,
+  faFolder,
+  faTasks,
+  faUser,
+  faEdit,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faHome, faFolder, faTasks, faUser, faEdit, faTimes, faInfoCircle);
