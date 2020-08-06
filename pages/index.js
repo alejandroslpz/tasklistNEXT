2095d8528d3a93e9cc4069333abec1bba2e2ceb8
Alejandro Sánchez <alejandroslpz@gmail.com>
2020-08-06 10:12:52 -0500

    Dinamismo con los dashboard y uso de states globales con context

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../components/Layout";
+import Panel from "../components/Panel";
+import { css } from "@emotion/core";
+import DashboardPrincipal from "../components/DashboardPrincipal";
+import DashboardUsuarios from "../components/DashboardUsuarios";
+import DashboardProyectos from "../components/proyectos/DashboardProyectos";
+import DashboardTareas from "../components/tareas/DashboardTareas";
+import appContext from "../context/app/appContext";
 
 const Index = () => {
+  const AppContext = useContext(appContext);
+  const { dashboard } = AppContext;
+
   return (
-    <Layout>
-      <div className="container">
-        <div className="columns">
-          <div className="column">
-            <h1 className="title is-1 has-text-centered my-6">Dashboard</h1>
+    <>
+      <Layout>
+        <div
+          className="columns"
+          css={css`
+            margin-top: 55px;
+          `}
+        >
+          <Panel />
+          <div className="column is-offset-2">
+            <div className="container">
+              {dashboard === "inicio" ? <DashboardPrincipal /> : null}
+              {dashboard === "proyectos" ? <DashboardProyectos /> : null}
+              {dashboard === "tareas" ? <DashboardTareas /> : null}
+              {dashboard === "usuarios" ? <DashboardUsuarios /> : null}
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 };
 
