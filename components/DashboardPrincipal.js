1d82b6b90a3187a9e6186e1a517c1d8da7434d45
Alejandro Sánchez <alejandroslpz@gmail.com>
2020-08-07 07:57:27 -0500

    Elemento doble eliminado

--- components/DashboardPrincipal.js
+++ components/DashboardPrincipal.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Tarea from "../components/Tarea";
 import Resumen from "../components/Resumen";
 
 const DashboardPrincipal = () => {
@@ -7,12 +6,8 @@ const DashboardPrincipal = () => {
     <>
       <h1 className="title is-1 has-text-centered my-6">Dashboard</h1>
       <div className="columns">
-        <div className="column is-6">
-          <Tarea />
-        </div>
-        <div className="column is-6">
-          <Tarea />
-        </div>
+        <div className="column is-6"></div>
+        <div className="column is-6"></div>
       </div>
       <div className="columns">
         <div className="column">
